f8212bb1a094f5b37b78c81d4876ae6bf924bca0
DNARoma <tangpei506@gmail.com>
2020-10-04 22:32:07 +0200

    feat: use hash router instead of history router

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 // import './index.css';
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
-import { BrowserRouter as Router} from "react-router-dom";
+import { HashRouter as Router} from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
