f9ab3f1057824271578271b3a6c5f6a2cc75da21
NonSpicyBurrito <burritoyangster@gmail.com>
2020-10-14 14:00:35 +0800

    feat: added sectioning to Home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -8,9 +8,11 @@ import {
   Tabs,
   // Theme,
   Typography,
+  Container,
   useMediaQuery,
   useTheme,
 } from "@material-ui/core";
+import { useLayoutStyles } from "../styles/layout";
 import { ColDef, DataGrid, ValueFormatterParams } from "@material-ui/data-grid";
 import {
   GitHub,
@@ -112,6 +114,7 @@ function InfoInternal(props: { onClick: () => void }) {
 function Home() {
   const theme = useTheme();
   const classes = useStyles();
+  const layoutClasses = useLayoutStyles();
   const { t } = useTranslation();
 
   const [informations] = useCachedData<IUserInformationInfo>(
@@ -191,75 +194,79 @@ function Home() {
 
   return (
     <Fragment>
-      {/* <Typography variant="h4">Welcome to Sekai Viewer Open Beta!</Typography> */}
-      <Alert className={classes.alert} severity="info">
-        {t("home:alert0")}
-      </Alert>
-      <Alert className={classes.alert} severity="info">
-        <Trans
-          i18nKey="home:alert_settings"
-          components={{ s: <Settings fontSize="inherit" /> }}
-        />
-      </Alert>
-      <Alert className={classes.alert} severity="warning">
-        <AlertTitle>{t("home:alert1.title")}</AlertTitle>
-        <Link
-          href="https://www.transifex.com/dnaroma/sekai-viewer"
-          target="_blank"
+      <Typography variant="h6" className={layoutClasses.header}>
+        {t("common:home")}
+      </Typography>
+      <Container className={layoutClasses.content} maxWidth="md">
+        {/* <Typography variant="h4">Welcome to Sekai Viewer Open Beta!</Typography> */}
+        <Alert className={classes.alert} severity="info">
+          {t("home:alert0")}
+        </Alert>
+        <Alert className={classes.alert} severity="info">
+          <Trans
+            i18nKey="home:alert_settings"
+            components={{ s: <Settings fontSize="inherit" /> }}
+          />
+        </Alert>
+        <Alert className={classes.alert} severity="warning">
+          <AlertTitle>{t("home:alert1.title")}</AlertTitle>
+          <Link
+            href="https://www.transifex.com/dnaroma/sekai-viewer"
+            target="_blank"
+          >
+            <Translate fontSize="inherit"></Translate>
+            {t("home:alert1.translation")}
+          </Link>
+          （English，简中，繁中，日本語，한국어，Deutsch, Español, others upon
+          request）
+          <br></br>
+          <Link
+            href="https://github.com/Sekai-World/sekai-viewer"
+            target="_blank"
+          >
+            <GitHub fontSize="inherit"></GitHub>
+            {t("home:alert1.development")}
+          </Link>
+          （Sekai-World/sekai-viewer）
+          <br></br>
+          {t("home:alert1.contact")}:
+          <Link
+            href="https://www.twitter.com/miku_zura"
+            target="_blank"
+            className={classes["contact-link"]}
+          >
+            <Twitter fontSize="inherit"></Twitter>
+            @miku_zura
+          </Link>
+          <Link href="#" className={classes["contact-link"]}>
+            <Discord fontSize="inherit"></Discord>
+            DNARoma#0646
+          </Link>
+        </Alert>
+        <Alert
+          className={classes.alert}
+          severity={getWebpDetectServerity(detected)}
         >
-          <Translate fontSize="inherit"></Translate>
-          {t("home:alert1.translation")}
-        </Link>
-        （English，简中，繁中，日本語，한국어，Deutsch, Español, others upon
-        request）
-        <br></br>
-        <Link
-          href="https://github.com/Sekai-World/sekai-viewer"
-          target="_blank"
-        >
-          <GitHub fontSize="inherit"></GitHub>
-          {t("home:alert1.development")}
-        </Link>
-        （Sekai-World/sekai-viewer）
-        <br></br>
-        {t("home:alert1.contact")}:
-        <Link
-          href="https://www.twitter.com/miku_zura"
-          target="_blank"
-          className={classes["contact-link"]}
-        >
-          <Twitter fontSize="inherit"></Twitter>
-          @miku_zura
-        </Link>
-        <Link href="#" className={classes["contact-link"]}>
-          <Discord fontSize="inherit"></Discord>
-          DNARoma#0646
-        </Link>
-      </Alert>
-      <Alert
-        className={classes.alert}
-        severity={getWebpDetectServerity(detected)}
-      >
-        <AlertTitle>WebP {t("common:support")}</AlertTitle>
-        <ul>
-          <li>WebP: {getWebpDetectDesc(t, detected.webp)}</li>
-          <li>WebP Lossless: {getWebpDetectDesc(t, detected.webpLossless)}</li>
-          <li>WebP Alpha: {getWebpDetectDesc(t, detected.webpAlpha)}</li>
-        </ul>
-        {getWebpDetectServerity(detected) === "success" ? (
-          <Typography>{t("home:detect.success")}</Typography>
-        ) : (
-          <Typography>
-            Unfortunately you will not able to load images normally. Please
-            consider update your browser version or use a modern browser. More
-            info:
-            <Link href="https://caniuse.com/webp">
-              Can I Use - WebP image format
-            </Link>
-          </Typography>
-        )}
-      </Alert>
-      <Paper>
+          <AlertTitle>WebP {t("common:support")}</AlertTitle>
+          <ul>
+            <li>WebP: {getWebpDetectDesc(t, detected.webp)}</li>
+            <li>WebP Lossless: {getWebpDetectDesc(t, detected.webpLossless)}</li>
+            <li>WebP Alpha: {getWebpDetectDesc(t, detected.webpAlpha)}</li>
+          </ul>
+          {getWebpDetectServerity(detected) === "success" ? (
+            <Typography>{t("home:detect.success")}</Typography>
+          ) : (
+            <Typography>
+              Unfortunately you will not able to load images normally. Please
+              consider update your browser version or use a modern browser. More
+              info:
+              <Link href="https://caniuse.com/webp">
+                Can I Use - WebP image format
+              </Link>
+            </Typography>
+          )}
+        </Alert>
+        <Paper>
         <Typography variant="h6" align="center">
           {t("home:game-news.title")}
         </Typography>
@@ -288,6 +295,7 @@ function Home() {
           ></DataGrid>
         </div>
       </Paper>
+      </Container>
       {info ? (
         <InfoInternalDialog
           url={`https://production-web.sekai.colorfulpalette.org/${info.path}`}
