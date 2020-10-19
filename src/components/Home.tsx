116c62f3fe2a723caea91f46c3b2d615b0c75885
DNARoma <tangpei506@gmail.com>
2020-10-19 12:24:32 +0200

    i18n: add more translator name

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -205,8 +205,10 @@ function Home() {
         <Alert className={classes.alert} severity="info">
           <AlertTitle>{t("home:alert_translate.title")}</AlertTitle>
           <ul>
-            <li>日: Passion, Cee</li>
+            <li>日: Passion, Cee, k0tayan</li>
+            <li>한：hodubidu3095, omitooshi, EleMas39, PJSEKAI</li>
             <li>Pt-BR: mid</li>
+            <li>русский: Spyrohat</li>
           </ul>
           <Trans
             i18nKey="home:alert_settings"
