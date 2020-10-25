47ec24bb85805e4781b7a60a85eb26637dc2a6b3
DNARoma <tangpei506@gmail.com>
2020-10-25 22:57:51 +0100

    refactor(Home): credit more translators

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -245,8 +245,8 @@ function Home() {
           <AlertTitle>{t("home:alert_translate.title")}</AlertTitle>
           <ul>
             <li>简：Stargazing Koishi</li>
-            <li>繁: Natsuzawa, ch ko</li>
-            <li>日: Passion, Cee, k0tayan, Natsuzawa</li>
+            <li>繁：Natsuzawa, ch ko</li>
+            <li>日：Passion, Cee, k0tayan, Natsuzawa, Build774</li>
             <li>한：hodubidu3095, omitooshi, EleMas39, PJSEKAI</li>
             <li>Pt-BR: mid</li>
             <li>русский: Spyrohat</li>
