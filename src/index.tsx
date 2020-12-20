d727868f31172d4295058088336ba3245f09612a
DNARoma <tangpei506@gmail.com>
2020-12-20 15:17:40 +0100

    chore: cache couintry code

--- src/index.tsx
+++ src/index.tsx
@@ -10,29 +10,46 @@ import { initGlobalI18n } from "./utils/i18n";
 import "./index.css";
 import { SettingProvider } from "./context";
 import Axios from "axios";
+import localforage from "localforage";
 
 TagManager.initialize({
   gtmId: "GTM-NFC6SW2",
 });
 
 window.isChinaMainland = false;
-Axios.get(`${process.env.REACT_APP_API_BACKEND_BASE}/country`).then((res) => {
-  window.isChinaMainland = res.data.data.country === "CN";
-  initGlobalI18n();
+localforage
+  .getItem<string>("country")
+  .then((country) => {
+    if (!country)
+      return Axios.get<{ data: { country: string } }>(
+        `${process.env.REACT_APP_API_BACKEND_BASE}/country`
+      );
+    else {
+      window.isChinaMainland = country === "CN";
+      initGlobalI18n();
+      return null;
+    }
+  })
+  .then((res) => {
+    if (res) {
+      window.isChinaMainland = res.data.data.country === "CN";
+      initGlobalI18n();
+      localforage.setItem<string>("country", res.data.data.country);
+    }
 
-  ReactDOM.render(
-    <React.StrictMode>
-      <Router>
-        <Suspense fallback="loading">
-          <SettingProvider>
-            <App />
-          </SettingProvider>
-        </Suspense>
-      </Router>
-    </React.StrictMode>,
-    document.getElementById("root")
-  );
-});
+    ReactDOM.render(
+      <React.StrictMode>
+        <Router>
+          <Suspense fallback="loading">
+            <SettingProvider>
+              <App />
+            </SettingProvider>
+          </Suspense>
+        </Router>
+      </React.StrictMode>,
+      document.getElementById("root")
+    );
+  });
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
