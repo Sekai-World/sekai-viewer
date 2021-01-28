0d8d7e95b81b88a2c1e0e18b62072529703e69ca
DNARoma <tangpei506@gmail.com>
2021-01-28 23:22:52 +0100

    feat(utils): add useLocalStorage and useToggle hooks

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,12 @@
 import Axios from "axios";
-import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import {
+  useCallback,
+  useEffect,
+  useMemo,
+  useReducer,
+  useRef,
+  useState,
+} from "react";
 import { WebpMachine } from "webp-hero";
 import PQueue from "p-queue";
 import localforage from "localforage";
@@ -492,4 +499,50 @@ export function getColorArray(num: number) {
     result.push(color);
   }
   return result;
+}
+
+export function useLocalStorage<T>(
+  key: string,
+  initialValue: T
+): [T, (value: T | ((val: T) => T)) => void] {
+  // State to store our value
+  // Pass initial state function to useState so logic is only executed once
+  const [storedValue, setStoredValue] = useState<T>(() => {
+    try {
+      // Get from local storage by key
+      const item = window.localStorage.getItem(key);
+      // Parse stored json or if none return initialValue
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      // If error also return initialValue
+      console.log(error);
+      return initialValue;
+    }
+  });
+
+  // Return a wrapped version of useState's setter function that ...
+  // ... persists the new value to localStorage.
+  const setValue = (value: T | ((val: T) => T)) => {
+    try {
+      // Allow value to be a function so we have same API as useState
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+      // Save state
+      setStoredValue(valueToStore);
+      // Save to local storage
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (error) {
+      // A more advanced implementation would handle the error case
+      console.log(error);
+    }
+  };
+
+  return [storedValue, setValue];
+}
+
+export function useToggle(initialValue = false) {
+  // Returns the tuple [state, dispatch]
+  // Normally with useReducer you pass a value to dispatch to indicate what action to
+  // take on the state, but in this case there's only one action.
+  return useReducer((state) => !state, initialValue);
 }
