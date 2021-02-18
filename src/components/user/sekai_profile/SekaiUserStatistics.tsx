aaa77bcca11846f60e267a5c80ab53404bf9147d
DNARoma <tangpei506@gmail.com>
2021-02-18 01:55:06 +0100

    feat(user): sekai profile music statistics

--- src/components/user/sekai_profile/SekaiUserStatistics.tsx
+++ src/components/user/sekai_profile/SekaiUserStatistics.tsx
@@ -1,20 +1,135 @@
-import { Button, Grid, Tooltip, Typography } from "@material-ui/core";
-import React, { useContext } from "react";
+import {
+  Button,
+  Grid,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Tooltip,
+  Typography,
+} from "@material-ui/core";
+import React, { useContext, useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import Image from "material-ui-image";
-import { UserContext } from "../../../context";
+import { SettingContext, UserContext } from "../../../context";
 import { charaIcons } from "../../../utils/resources";
 import DegreeImage from "../../subs/DegreeImage";
-import { useCachedData, useToggle } from "../../../utils";
-import { IAreaItem } from "../../../types";
+import { getRemoteAssetURL, useCachedData, useToggle } from "../../../utils";
+import {
+  IAreaItem,
+  IMusicInfo,
+  UserMusic,
+  UserMusicDifficultyStatus,
+} from "../../../types";
+import { useAssetI18n } from "../../../utils/i18n";
+
+import BtnDifficultyEasy from "../../../assets/music/btn_difficulty_easy.png";
+import BtnDifficultyNormal from "../../../assets/music/btn_difficulty_normal.png";
+import BtnDifficultyHard from "../../../assets/music/btn_difficulty_hard.png";
+import BtnDifficultyExpert from "../../../assets/music/btn_difficulty_expert.png";
+import BtnDifficultyMaster from "../../../assets/music/btn_difficulty_master.png";
+
+import IconNotClear from "../../../assets/music/icon_notClear.png";
+import IconClear from "../../../assets/music/icon_clear.png";
+import IconFullCombo from "../../../assets/music/icon_fullCombo.png";
+import IconAllPerfect from "../../../assets/music/icon_allPerfect.png";
+
+const ProfileMusicImage: React.FC<{
+  assetbundleName: string;
+  title: string;
+}> = ({ assetbundleName, title }) => {
+  const [img, setImg] = useState<string>("");
+
+  useEffect(() => {
+    if (!img)
+      getRemoteAssetURL(
+        `music/jacket/${assetbundleName}_rip/${assetbundleName}.webp`,
+        setImg
+      );
+  }, [assetbundleName, img]);
+
+  return <Image src={img} alt={title} aspectRatio={1} color=""></Image>;
+};
+
+const getMusicClearIcon = (status: UserMusicDifficultyStatus) => {
+  const isClear = status.userMusicResults.reduce(
+    (sum, elem) => sum || elem.playResult === "clear",
+    false
+  );
+  const isFullCombo = status.userMusicResults.reduce(
+    (sum, elem) => sum || elem.fullComboFlg,
+    false
+  );
+  const isAllPerfect = status.userMusicResults.reduce(
+    (sum, elem) => sum || elem.fullPerfectFlg,
+    false
+  );
+
+  if (!isClear && !isFullCombo && !isAllPerfect) return IconNotClear;
+  else if (isClear && !isFullCombo && !isAllPerfect) return IconClear;
+  else if (isFullCombo && !isAllPerfect) return IconFullCombo;
+  else if (isFullCombo && isAllPerfect) return IconAllPerfect;
+};
+
+const getClearCount = (idx: number, userMusics?: UserMusic[]) => {
+  if (!userMusics) return 0;
+  return userMusics.reduce((sum, umusic) => {
+    const status = umusic.userMusicDifficultyStatuses[idx];
+    const isClear = status.userMusicResults.reduce(
+      (sum, elem) => sum || elem.playResult === "clear",
+      false
+    );
+    const isFullCombo = status.userMusicResults.reduce(
+      (sum, elem) => sum || elem.fullComboFlg,
+      false
+    );
+    const isAllPerfect = status.userMusicResults.reduce(
+      (sum, elem) => sum || elem.fullPerfectFlg,
+      false
+    );
+
+    return sum + Number(isClear || isFullCombo || isAllPerfect);
+  }, 0);
+};
+
+const getFullComboCount = (idx: number, userMusics?: UserMusic[]) => {
+  if (!userMusics) return 0;
+  return userMusics.reduce((sum, umusic) => {
+    const status = umusic.userMusicDifficultyStatuses[idx];
+    const isFullCombo = status.userMusicResults.reduce(
+      (sum, elem) => sum || elem.playResult === "full_combo",
+      false
+    );
+
+    return sum + Number(isFullCombo);
+  }, 0);
+};
+
+const getAllPerfectCount = (idx: number, userMusics?: UserMusic[]) => {
+  if (!userMusics) return 0;
+  return userMusics.reduce((sum, umusic) => {
+    const status = umusic.userMusicDifficultyStatuses[idx];
+    const isAllPerfect = status.userMusicResults.reduce(
+      (sum, elem) => sum || elem.playResult === "full_perfect",
+      false
+    );
+
+    return sum + Number(isAllPerfect);
+  }, 0);
+};
 
 const SekaiUserStatistics = () => {
   // const layoutClasses = useLayoutStyles();
   // const interactiveClasses = useInteractiveStyles();
   const { t } = useTranslation();
   const { sekaiProfile } = useContext(UserContext)!;
+  const { getTranslated } = useAssetI18n();
+  const { contentTransMode } = useContext(SettingContext)!;
 
   const [areaItems] = useCachedData<IAreaItem>("areaItems");
+  const [musics] = useCachedData<IMusicInfo>("musics");
 
   const [characterRankOpen, toggleCharacterRankOpen] = useToggle(false);
   const [honorOpen, toggleHonorOpen] = useToggle(false);
@@ -134,10 +249,256 @@ const SekaiUserStatistics = () => {
           variant="contained"
           color="primary"
           onClick={() => toggleMusicOpen()}
-          disabled
         >
           {musicOpen ? t("common:hide") : t("common:show")}
         </Button>
+        {musicOpen && musics && (
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TableContainer>
+                <Table>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>{t("music:difficulty")}</TableCell>
+                      <TableCell>
+                        <Image
+                          src={BtnDifficultyEasy}
+                          alt="difficulty easy"
+                          color=""
+                        />
+                      </TableCell>
+                      <TableCell>
+                        <Image
+                          src={BtnDifficultyNormal}
+                          alt="difficulty normal"
+                          color=""
+                        />
+                      </TableCell>
+                      <TableCell>
+                        <Image
+                          src={BtnDifficultyHard}
+                          alt="difficulty hard"
+                          color=""
+                        />
+                      </TableCell>
+                      <TableCell>
+                        <Image
+                          src={BtnDifficultyExpert}
+                          alt="difficulty expert"
+                          color=""
+                        />
+                      </TableCell>
+                      <TableCell>
+                        <Image
+                          src={BtnDifficultyMaster}
+                          alt="difficulty master"
+                          color=""
+                        />
+                      </TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    <TableRow>
+                      <TableCell>
+                        {t("user:profile.statistics.clear_count")}
+                      </TableCell>
+                      {Array.from({ length: 5 }).map((_, idx) => (
+                        <TableCell key={idx} align="center">
+                          <div style={{ minWidth: "64px" }}>
+                            {getClearCount(
+                              idx,
+                              sekaiProfile?.sekaiUserProfile?.userMusics
+                            )}
+                          </div>
+                        </TableCell>
+                      ))}
+                    </TableRow>
+                    <TableRow>
+                      <TableCell>
+                        {t("user:profile.statistics.full_combo_count")}
+                      </TableCell>
+                      {Array.from({ length: 5 }).map((_, idx) => (
+                        <TableCell key={idx} align="center">
+                          <div style={{ minWidth: "64px" }}>
+                            {getFullComboCount(
+                              idx,
+                              sekaiProfile?.sekaiUserProfile?.userMusics
+                            )}
+                          </div>
+                        </TableCell>
+                      ))}
+                    </TableRow>
+                    <TableRow>
+                      <TableCell>
+                        {t("user:profile.statistics.all_perfect_count")}
+                      </TableCell>
+                      {Array.from({ length: 5 }).map((_, idx) => (
+                        <TableCell key={idx} align="center">
+                          <div style={{ minWidth: "64px" }}>
+                            {getAllPerfectCount(
+                              idx,
+                              sekaiProfile?.sekaiUserProfile?.userMusics
+                            )}
+                          </div>
+                        </TableCell>
+                      ))}
+                    </TableRow>
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </Grid>
+            {sekaiProfile?.sekaiUserProfile?.userMusics.map((umusic) => (
+              <Grid key={umusic.musicId} item xs={12}>
+                <Grid
+                  container
+                  justify="center"
+                  alignItems="center"
+                  spacing={1}
+                >
+                  <Grid item xs={8} md={3}>
+                    <ProfileMusicImage
+                      assetbundleName={
+                        musics.find((m) => m.id === umusic.musicId)!
+                          .assetbundleName
+                      }
+                      title={getTranslated(
+                        contentTransMode,
+                        `music_titles:${umusic.musicId}`,
+                        musics.find((m) => m.id === umusic.musicId)!.title
+                      )}
+                    />
+                  </Grid>
+                  <Grid item xs={12} md={9}>
+                    <TableContainer>
+                      <Table>
+                        <TableHead>
+                          <TableRow>
+                            <TableCell>{t("music:difficulty")}</TableCell>
+                            <TableCell>
+                              <Image
+                                src={BtnDifficultyEasy}
+                                alt="difficulty easy"
+                                color=""
+                              />
+                            </TableCell>
+                            <TableCell>
+                              <Image
+                                src={BtnDifficultyNormal}
+                                alt="difficulty normal"
+                                color=""
+                              />
+                            </TableCell>
+                            <TableCell>
+                              <Image
+                                src={BtnDifficultyHard}
+                                alt="difficulty hard"
+                                color=""
+                              />
+                            </TableCell>
+                            <TableCell>
+                              <Image
+                                src={BtnDifficultyExpert}
+                                alt="difficulty expert"
+                                color=""
+                              />
+                            </TableCell>
+                            <TableCell>
+                              <Image
+                                src={BtnDifficultyMaster}
+                                alt="difficulty master"
+                                color=""
+                              />
+                            </TableCell>
+                          </TableRow>
+                        </TableHead>
+                        <TableBody>
+                          <TableRow>
+                            <TableCell>{t("music:clear_status")}</TableCell>
+                            {umusic.userMusicDifficultyStatuses.map(
+                              (status) => (
+                                <TableCell
+                                  key={status.musicDifficulty}
+                                  align="center"
+                                >
+                                  <div style={{ minWidth: "64px" }}>
+                                    <img
+                                      src={getMusicClearIcon(status)}
+                                      alt="clear icon"
+                                    />
+                                  </div>
+                                </TableCell>
+                              )
+                            )}
+                          </TableRow>
+                          <TableRow>
+                            <TableCell>{t("music:hi_score")}</TableCell>
+                            {umusic.userMusicDifficultyStatuses.map(
+                              (status) => (
+                                <TableCell
+                                  key={status.musicDifficulty}
+                                  align="center"
+                                >
+                                  {status.userMusicResults.length &&
+                                  status.userMusicResults.find(
+                                    (umr) => umr.playType === "multi"
+                                  )
+                                    ? status.userMusicResults.find(
+                                        (umr) => umr.playType === "multi"
+                                      )!.highScore
+                                    : "N/A"}
+                                </TableCell>
+                              )
+                            )}
+                          </TableRow>
+                          <TableRow>
+                            <TableCell>{t("music:mvp_count")}</TableCell>
+                            {umusic.userMusicDifficultyStatuses.map(
+                              (status) => (
+                                <TableCell
+                                  key={status.musicDifficulty}
+                                  align="center"
+                                >
+                                  {status.userMusicResults.length &&
+                                  status.userMusicResults.find(
+                                    (umr) => umr.playType === "multi"
+                                  )
+                                    ? status.userMusicResults.find(
+                                        (umr) => umr.playType === "multi"
+                                      )!.mvpCount
+                                    : "N/A"}
+                                </TableCell>
+                              )
+                            )}
+                          </TableRow>
+                          <TableRow>
+                            <TableCell>{t("music:super_star_count")}</TableCell>
+                            {umusic.userMusicDifficultyStatuses.map(
+                              (status) => (
+                                <TableCell
+                                  key={status.musicDifficulty}
+                                  align="center"
+                                >
+                                  {status.userMusicResults.length &&
+                                  status.userMusicResults.find(
+                                    (umr) => umr.playType === "multi"
+                                  )
+                                    ? status.userMusicResults.find(
+                                        (umr) => umr.playType === "multi"
+                                      )!.superStarCount
+                                    : "N/A"}
+                                </TableCell>
+                              )
+                            )}
+                          </TableRow>
+                        </TableBody>
+                      </Table>
+                    </TableContainer>
+                  </Grid>
+                </Grid>
+              </Grid>
+            ))}
+          </Grid>
+        )}
       </Grid>
     </Grid>
   );
