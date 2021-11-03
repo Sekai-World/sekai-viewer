931fdb6d1ae83e2c5e5bd56cd5e093efe8208761
DNARoma <tangpei506@gmail.com>
2021-11-04 00:29:38 +0100

    build(vite): :zap: add manualChunks to do vendor code splitting

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,27 @@ import svgr from "vite-plugin-svgr";
 import Icons from "unplugin-icons/vite";
 import { VitePWA } from "vite-plugin-pwa";
 
+import { dependencies } from "./package.json";
+function renderChunks(deps: Record<string, string>) {
+  let chunks = {};
+  Object.keys(deps).forEach((key) => {
+    if (["react", "react-router-dom", "react-dom"].includes(key)) return;
+    chunks[key] = [key];
+  });
+  return chunks;
+}
+
 export default defineConfig({
   build: {
     outDir: "build",
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-router-dom", "react-dom"],
+          ...renderChunks(dependencies),
+        },
+      },
+    },
   },
   plugins: [
     reactRefresh(),
