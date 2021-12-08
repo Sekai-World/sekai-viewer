e6c5ebcb4150de4b9c6462ee686fee2e2bc95e0a
DNARoma <tangpei506@gmail.com>
2021-12-08 22:13:16 +0100

    fix(user): :bug: event record filtering event

--- src/pages/user/sekai_profile/SekaiEventRecord.tsx
+++ src/pages/user/sekai_profile/SekaiEventRecord.tsx
@@ -48,8 +48,9 @@ const SekaiEventRecord = observer((props: Props) => {
     id: number;
   } | null>(null);
   // const [currentEvent, setCurrentEvent] = useState<SekaiCurrentEventModel>();
-  const [eventRecords, setEventRecords] =
-    useState<SekaiProfileEventRecordModel[]>();
+  const [eventRecords, setEventRecords] = useState<
+    SekaiProfileEventRecordModel[]
+  >([]);
   const [isEventRecording, setIsEventRecording] = useState(false);
   const [sekaiProfile, setLocalSekaiProfile] = useState<ISekaiProfile>();
 
@@ -62,8 +63,15 @@ const SekaiEventRecord = observer((props: Props) => {
   useEffect(() => {
     const func = async () => {
       // console.log(currEvent);
-      if (props.eventId) {
+      if (props.eventId && events) {
         try {
+          const ev = events.find((elem) => elem.id === props.eventId);
+          if (ev) {
+            setSelectedEvent({
+              name: getTranslated(`event_name:${currEvent.eventId}`, ev.name),
+              id: ev.id,
+            });
+          }
           setEventRecords(await getSekaiProfileEventRecordMe(props.eventId));
         } catch (error) {
           setEventRecords([]);
@@ -98,63 +106,71 @@ const SekaiEventRecord = observer((props: Props) => {
 
   return (
     <Grid container direction="column" spacing={1}>
-      <Grid item container spacing={1} alignItems="center">
+      {!props.eventId && (
         <Grid item>
-          <Autocomplete
-            options={(events || [])
-              .slice()
-              .reverse()
-              .map((ev) => ({
-                name: getTranslated(`event_name:${ev.id}`, ev.name),
-                id: ev.id,
-              }))}
-            getOptionLabel={(option) => option.name}
-            isOptionEqualToValue={(option, value) => option.id === value.id}
-            renderInput={(params) => (
-              <TextField
-                {...params}
-                label={t("event:tracker.select.event_name")}
+          <Grid container spacing={1} alignItems="center">
+            <Grid item>
+              <Autocomplete
+                options={(events || [])
+                  .filter((event) => event.startAt <= new Date().getTime())
+                  .slice()
+                  .reverse()
+                  .map((ev) => ({
+                    name: getTranslated(`event_name:${ev.id}`, ev.name),
+                    id: ev.id,
+                  }))}
+                getOptionLabel={(option) => option.name}
+                isOptionEqualToValue={(option, value) => option.id === value.id}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    label={t("event:tracker.select.event_name")}
+                  />
+                )}
+                value={selectedEvent}
+                autoComplete
+                onChange={async (_, value) => {
+                  setSelectedEvent(value);
+                  setEventRecords([]);
+                  if (value)
+                    setEventRecords(
+                      await getSekaiProfileEventRecordMe(value.id)
+                    );
+                }}
+                disabled={isCurrEventLoading || isEventRecording}
+                style={{
+                  minWidth: "250px",
+                }}
               />
-            )}
-            value={selectedEvent}
-            autoComplete
-            onChange={async (_, value) => {
-              setSelectedEvent(value);
-              setEventRecords([]);
-              if (value)
-                setEventRecords(await getSekaiProfileEventRecordMe(value.id));
-            }}
-            disabled={isCurrEventLoading || isEventRecording}
-            style={{
-              minWidth: "250px",
-            }}
-          />
+            </Grid>
+            <Grid item>
+              <Button
+                variant="contained"
+                onClick={async () => {
+                  setSelectedEvent({
+                    name: getTranslated(
+                      `event_name:${currEvent.eventId}`,
+                      currEvent.eventJson.name
+                    ),
+                    id: currEvent.eventId,
+                  });
+                  setEventRecords([]);
+                  setEventRecords(
+                    await getSekaiProfileEventRecordMe(currEvent.eventId)
+                  );
+                }}
+                disabled={!currEvent || isCurrEventLoading || isEventRecording}
+                size="large"
+              >
+                {t("event:tracker.button.curr_event")}
+              </Button>
+            </Grid>
+          </Grid>
         </Grid>
-        <Grid item>
-          <Button
-            variant="contained"
-            onClick={async () => {
-              setSelectedEvent({
-                name: getTranslated(
-                  `event_name:${currEvent.eventId}`,
-                  currEvent.eventJson.name
-                ),
-                id: currEvent.eventId,
-              });
-              setEventRecords([]);
-              setEventRecords(
-                await getSekaiProfileEventRecordMe(currEvent.eventId)
-              );
-            }}
-            disabled={isCurrEventLoading || isEventRecording}
-            size="large"
-          >
-            {t("event:tracker.button.curr_event")}
-          </Button>
-        </Grid>
-      </Grid>
+      )}
       <Grid item container spacing={1} alignItems="center">
         {!!selectedEvent &&
+          !!currEvent &&
           selectedEvent.id === currEvent.eventId &&
           !!sekaiProfile && (
             <Grid item>
