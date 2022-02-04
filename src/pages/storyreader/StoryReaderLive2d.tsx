612d4c6aa6de2499a5ca7a5335c9ab371a3b4375
kyokuheishin <kyokuheishin@gmail.com>
2022-02-04 17:39:37 +0900

    feat(Story Reader Live2d): add StoryReaderLive2d.tsx

---
+++ src/pages/storyreader/StoryReaderLive2d.tsx
@@ -0,0 +1,9 @@
+/* eslint-disable prettier/prettier */
+import { Container } from "@mui/material";
+import React from "react";
+
+const StoryReaderLive2d: React.FC = () => {
+    return <Container></Container>;
+};
+
+export default StoryReaderLive2d;
