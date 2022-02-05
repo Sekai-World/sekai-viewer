4c93cb3a532ae6b0ad9cb07258d9834a4cc4a6c9
kyokuheishin <kyokuheishin@gmail.com>
2022-02-05 18:20:31 +0900

    feat(Story Reader Live2d): use useProcessedScenarioDataForLive2d()

--- src/pages/storyreader/StoryReaderContentLive2d.tsx
+++ src/pages/storyreader/StoryReaderContentLive2d.tsx
@@ -13,10 +13,16 @@ import {
   ISpecialStory,
 } from "../../types.d";
 import {
+  getRemoteAssetURL,
   useAlertSnackbar,
   useCachedData,
   useProcessedScenarioData,
+  useProcessedScenarioDataForLive2d,
 } from "../../utils";
+import { charaIcons } from "../../utils/resources";
+import { ReleaseCondTrans } from "../../components/helpers/ContentTrans";
+import { Sound, SpecialEffect, Talk } from "./StoryReaderSnippet";
+import Image from "mui-image";
 import { useAssetI18n } from "../../utils/i18n";
 
 const useStyle = makeStyles((theme) => ({
@@ -33,7 +39,7 @@ const StoryReaderContentLive2d: React.FC<{
   const classes = useStyle();
   const { t } = useTranslation();
   const { getTranslated } = useAssetI18n();
-  const getProcessedScenarioData = useProcessedScenarioData();
+  const getProcessedScenarioDataForLive2d = useProcessedScenarioDataForLive2d();
   const { showError } = useAlertSnackbar();
 
   const [unitStories] = useCachedData<IUnitStory>("unitStories");
@@ -47,10 +53,212 @@ const StoryReaderContentLive2d: React.FC<{
 
   const [chapterTitle, setChapterTitle] = useState<string>("");
   const [episodeTitle, setEpisodeTitle] = useState<string>("");
+  const [scenarioData, setScenarioData] = useState<{
+    characters: {
+      id: number;
+      name: string;
+    }[];
+    actions: {
+      [key: string]: any;
+    }[];
+  }>({
+    characters: [],
+    actions: [],
+  });
 
   const [releaseConditionId, setReleaseConditionId] = useState<number>(0);
 
-  useEffect(() => {});
+  useEffect(() => {
+    setScenarioData({
+      characters: [],
+      actions: [],
+    });
+    try {
+      switch (storyType) {
+        case "unitStory":
+          if (unitStories) {
+            const [, , , unitId, chapterNo, episodeNo] = storyId.split("/");
+
+            const chapter = unitStories
+              .find((us) => us.unit === unitId)!
+              .chapters.find((ch) => ch.chapterNo === Number(chapterNo))!;
+
+            const episode = chapter.episodes.find(
+              (ep) => ep.episodeNo === Number(episodeNo)
+            )!;
+
+            getRemoteAssetURL(
+              `story/episode_image/${chapter.assetbundleName}_rip/${episode.assetbundleName}.webp`,
+              setBannerUrl,
+              window.isChinaMainland ? "cn" : "ww"
+            );
+            getProcessedScenarioDataForLive2d(
+              `scenario/unitstory/${chapter.assetbundleName}_rip/${episode.scenarioId}.asset`,
+              false
+            ).then((data) => setScenarioData(data));
+
+            setChapterTitle(
+              getTranslated(
+                `unit_story_chapter_title:${chapter.unit}-${chapter.chapterNo}`,
+                chapter.title
+              )
+            );
+            setEpisodeTitle(
+              getTranslated(
+                `unit_story_episode_title:${episode.unit}-${episode.chapterNo}-${episode.episodeNo}`,
+                episode.title
+              )
+            );
+            setReleaseConditionId(episode.releaseConditionId);
+          }
+          break;
+        case "eventStory":
+          if (eventStories) {
+            const [, , , eventId, episodeNo] = storyId.split("/");
+
+            const chapter = eventStories.find(
+              (es) => es.eventId === Number(eventId)
+            )!;
+
+            const episode = chapter.eventStoryEpisodes.find(
+              (ep) => ep.episodeNo === Number(episodeNo)
+            )!;
+
+            getRemoteAssetURL(
+              `event_story/${chapter.assetbundleName}/episode_image_rip/${episode.assetbundleName}.webp`,
+              setBannerUrl,
+              window.isChinaMainland ? "cn" : "ww"
+            );
+            getProcessedScenarioDataForLive2d(
+              `event_story/${chapter.assetbundleName}/scenario_rip/${episode.scenarioId}.asset`,
+              false
+            ).then((data) => setScenarioData(data));
+
+            setChapterTitle("");
+            setEpisodeTitle(
+              `${episode.episodeNo} - ${getTranslated(
+                `event_story_episode_title:${episode.eventStoryId}-${episode.episodeNo}`,
+                episode.title
+              )}`
+            );
+            setReleaseConditionId(episode.releaseConditionId);
+          }
+          break;
+        case "charaStory":
+          if (characterProfiles) {
+            const [, , , charaId] = storyId.split("/");
+
+            const episode = characterProfiles.find(
+              (cp) => cp.characterId === Number(charaId)
+            )!;
+
+            setBannerUrl(charaIcons[`CharaIcon${charaId}` as "CharaIcon1"]);
+            getProcessedScenarioDataForLive2d(
+              `scenario/profile_rip/${episode.scenarioId}.asset`,
+              false
+            ).then((data) => setScenarioData(data));
+
+            setChapterTitle("");
+            setEpisodeTitle(t("member:introduction"));
+            setReleaseConditionId(0);
+          }
+          break;
+        case "cardStory":
+          if (cardEpisodes) {
+            const [, , , , , cardEpisodeId] = storyId.split("/");
+
+            const episode = cardEpisodes.find(
+              (ce) => ce.id === Number(cardEpisodeId)
+            )!;
+
+            // setBannerUrl(charaIcons[`CharaIcon${charaId}` as "CharaIcon1"]);
+            getRemoteAssetURL(
+              `character/member_small/${episode.assetbundleName}_rip/card_normal.webp`,
+              setBannerUrl,
+              window.isChinaMainland ? "cn" : "ww"
+            );
+            getProcessedScenarioDataForLive2d(
+              `character/member/${episode.assetbundleName}_rip/${episode.scenarioId}.asset`,
+              true
+            ).then((data) => setScenarioData(data));
+
+            setChapterTitle("");
+            setEpisodeTitle(
+              getTranslated(
+                `card_episode_title:${episode.title}`,
+                episode.title
+              )
+            );
+            setReleaseConditionId(episode.releaseConditionId);
+          }
+          break;
+        case "areaTalk":
+          if (actionSets) {
+            const [, , , , actionSetId] = storyId.split("/");
+
+            const episode = actionSets.find(
+              (as) => as.id === Number(actionSetId)
+            )!;
+
+            // getRemoteAssetURL(
+            //   `character/member_small/${episode.assetbundleName}_rip/card_normal.webp`,
+            //   setBannerUrl,
+            //   window.isChinaMainland
+            // );
+            getProcessedScenarioDataForLive2d(
+              `scenario/actionset/group${Math.floor(episode.id / 100)}_rip/${
+                episode.scenarioId
+              }.asset`,
+              false,
+              true
+            ).then((data) => setScenarioData(data));
+
+            setChapterTitle("");
+            setEpisodeTitle("");
+            // setReleaseConditionId(episode.releaseConditionId);
+          }
+          break;
+        case "specialStory":
+          if (specialStories) {
+            const [, , , spId, episodeNo] = storyId.split("/");
+            const chapter = specialStories.find((sp) => sp.id === Number(spId));
+            const episode = chapter?.episodes.find(
+              (ep) => ep.episodeNo === Number(episodeNo)
+            );
+
+            if (episode?.scenarioId.startsWith("op"))
+              getProcessedScenarioDataForLive2d(
+                `scenario/special/${chapter?.assetbundleName}_rip/${episode?.scenarioId}.asset`,
+                false
+              ).then((data) => setScenarioData(data));
+            else
+              getProcessedScenarioDataForLive2d(
+                `scenario/special/${episode?.assetbundleName}_rip/${episode?.scenarioId}.asset`,
+                false
+              ).then((data) => setScenarioData(data));
+
+            setChapterTitle(chapter?.title || "");
+            setEpisodeTitle(episode?.title || "");
+          }
+          break;
+      }
+    } catch (error) {
+      showError("failed to load episode");
+    }
+  }, [
+    unitStories,
+    eventStories,
+    storyId,
+    storyType,
+    getProcessedScenarioDataForLive2d,
+    characterProfiles,
+    cardEpisodes,
+    t,
+    getTranslated,
+    showError,
+    actionSets,
+    specialStories,
+  ]);
 
   return <Container></Container>;
 };
