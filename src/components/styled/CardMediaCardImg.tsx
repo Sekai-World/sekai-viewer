b3c421288e98fdb7ba39a69c82be7aa9404ec837
dnaroma <tangpei506@gmail.com>
2022-09-27 23:02:14 +0200

    fix: :lipstick: card image style not working

--- src/components/styled/CardMediaCardImg.tsx
+++ src/components/styled/CardMediaCardImg.tsx
@@ -1,9 +1,22 @@
-import { CardMedia } from "@mui/material";
+import { Box, CardMedia, CardMediaProps } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
-export default styled(CardMedia)`
-  & .MuiCardMedia-root {
-    padding-top: 56.25%;
-    cursor: pointer;
-  }
-`;
+import React from "react";
+
+type Props = CardMediaProps;
+
+const CardMediaCardImg = (props: Props) => {
+  const Root = styled(Box)`
+    & .card-img-root {
+      padding-top: 56.25%;
+      cursor: pointer;
+    }
+  `;
+  return (
+    <Root>
+      <CardMedia classes={{ root: "card-img-root" }} {...props} />
+    </Root>
+  );
+};
+
+export default CardMediaCardImg;
