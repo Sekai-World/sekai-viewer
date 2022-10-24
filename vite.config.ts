f0d4d1dda291333219481f768722151b34d97271
33 <moe@3-3.dev>
2022-10-24 16:38:29 +0800

    fix: 403 and CORS problem for Mainland China developer

--- vite.config.ts
+++ vite.config.ts
@@ -90,6 +90,8 @@ export default defineConfig({
     },
   },
   server: {
+    host: "127.0.0.1",
+    port: 3000,
     proxy: {
       "/sekai-assets": {
         target: "https://storage.sekai.best",
