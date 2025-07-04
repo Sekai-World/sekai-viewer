fe5a2dd250fcafe8245ecd57c74f5f19a243d0fb
dnaroma <me@dna.moe>
2025-07-04 16:49:01 +0200

    feat(event): add filtering options for event type and start time

--- src/pages/event/EventListFilter.tsx
+++ src/pages/event/EventListFilter.tsx
@@ -1,12 +1,24 @@
-import React, { useMemo, useState } from "react";
+import React, { useCallback, useMemo, useState } from "react";
 import { EventType } from "../../types";
-import { Button, Collapse, FormControl, Grid, TextField } from "@mui/material";
+import {
+  Button,
+  Chip,
+  Collapse,
+  FormControl,
+  Grid,
+  MenuItem,
+  Select,
+  TextField,
+} from "@mui/material";
 import PaperContainer from "../../components/styled/PaperContainer";
 import TypographyCaption from "../../components/styled/TypographyCaption";
 import { Check, ClearAll, RotateLeft } from "@mui/icons-material";
 import { useTranslation } from "react-i18next";
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import dayjs from "dayjs";
 
-type EventFilterStartAtType = "before" | "after";
+type EventFilterStartAtType = "before" | "after" | undefined;
 
 type EventFilterEventUnitType = "event" | "eventStory";
 
@@ -15,17 +27,37 @@ type EventFilterInclExclType = "incl" | "excl" | "both";
 export interface EventFilterData {
   searchTitle: string;
   eventType: EventType[];
-  startAtType: EventFilterStartAtType | null;
-  startAt: number | null;
+  startAtType: EventFilterStartAtType | undefined;
+  startAt: number | undefined;
   // There are units in the event.json, as well as in the eventStory.json
   // eventUnitType is used to distinguish between the two
-  eventUnitType: EventFilterEventUnitType | null;
+  eventUnitType: EventFilterEventUnitType | undefined;
   eventUnit: string[];
   isKeyEventStory: EventFilterInclExclType;
   hasEventMusic: EventFilterInclExclType;
   eventBonusAttr: string[];
   eventBonusUnitId: number[];
 }
+
+const eventTypes = Object.freeze([
+  {
+    value: "marathon",
+    label: "event:type.marathon",
+  },
+  {
+    value: "cheerful_carnival",
+    label: "event:type.cheerful_carnival",
+  },
+  // Not Implemented Yet
+  // {
+  //   value: "challenge_live",
+  //   label: "Challenge Live",
+  // },
+  {
+    value: "world_bloom",
+    label: "event:type.world_bloom",
+  },
+]);
 
 const EventListFilter: React.FC<{
   filterOpened: boolean;
@@ -39,12 +71,13 @@ const EventListFilter: React.FC<{
     filterData.searchTitle
   );
   const [eventType, setEventType] = useState<EventType[]>([]);
-  const [startAtType, setStartAtType] = useState<EventFilterStartAtType | null>(
-    null
-  );
-  const [startAt, setStartAt] = useState<number | null>(null);
-  const [eventUnitType, setEventUnitType] =
-    useState<EventFilterEventUnitType | null>(null);
+  const [startAtType, setStartAtType] = useState<
+    EventFilterStartAtType | undefined
+  >();
+  const [startAt, setStartAt] = useState<number | undefined>();
+  const [eventUnitType, setEventUnitType] = useState<
+    EventFilterEventUnitType | undefined
+  >();
   const [eventUnit, setEventUnit] = useState<string[]>([]);
   const [isKeyEventStory, setIsKeyEventStory] =
     useState<EventFilterInclExclType>("both");
@@ -56,13 +89,18 @@ const EventListFilter: React.FC<{
   const isFilterDataChanged = useMemo(() => {
     return searchTitle !== filterData.searchTitle;
   }, [searchTitle, filterData.searchTitle]);
-  const isFilterNotEmpty = Object.values(filterData).some((value) => {
-    if (Array.isArray(value)) return value.length > 0;
-    if (typeof value === "string") return value.trim() !== "";
-    return value !== null && value !== undefined;
-  });
+  const isFilterNotEmpty = useMemo(
+    () =>
+      Object.values(filterData).some((value) => {
+        if (Array.isArray(value)) return value.length > 0;
+        if (typeof value === "string")
+          return value.trim() !== "" && value !== "both";
+        return value !== null && value !== undefined;
+      }),
+    [filterData]
+  );
 
-  const applyFilter = () => {
+  const applyFilter = useCallback(() => {
     onFilterDataChange({
       ...filterData,
       searchTitle: searchTitle,
@@ -77,9 +115,23 @@ const EventListFilter: React.FC<{
       eventBonusUnitId: eventBonusUnitId,
     });
     toggleFilterOpened();
-  };
+  }, [
+    eventBonusAttr,
+    eventBonusUnitId,
+    eventType,
+    eventUnit,
+    eventUnitType,
+    filterData,
+    hasEventMusic,
+    isKeyEventStory,
+    onFilterDataChange,
+    searchTitle,
+    startAt,
+    startAtType,
+    toggleFilterOpened,
+  ]);
 
-  const resetToPropFilterData = () => {
+  const resetToPropFilterData = useCallback(() => {
     setSearchTitle(filterData.searchTitle);
     setEventType(filterData.eventType);
     setStartAtType(filterData.startAtType);
@@ -90,20 +142,41 @@ const EventListFilter: React.FC<{
     setHasEventMusic(filterData.hasEventMusic);
     setEventBonusAttr(filterData.eventBonusAttr);
     setEventBonusUnitId(filterData.eventBonusUnitId);
-  };
+  }, [
+    filterData.eventBonusAttr,
+    filterData.eventBonusUnitId,
+    filterData.eventType,
+    filterData.eventUnit,
+    filterData.eventUnitType,
+    filterData.hasEventMusic,
+    filterData.isKeyEventStory,
+    filterData.searchTitle,
+    filterData.startAt,
+    filterData.startAtType,
+  ]);
 
-  const clearFilterData = () => {
+  const clearFilterData = useCallback(() => {
     setSearchTitle("");
     setEventType([]);
-    setStartAtType(null);
-    setStartAt(null);
-    setEventUnitType(null);
+    setStartAtType(undefined);
+    setStartAt(undefined);
+    setEventUnitType(undefined);
     setEventUnit([]);
     setIsKeyEventStory("both");
     setHasEventMusic("both");
     setEventBonusAttr([]);
     setEventBonusUnitId([]);
-  };
+  }, []);
+
+  const handleEventTypeClick = useCallback((type: EventType) => {
+    setEventType((prev) => {
+      if (prev.includes(type)) {
+        return prev.filter((t) => t !== type);
+      } else {
+        return [...prev, type];
+      }
+    });
+  }, []);
 
   return (
     <Collapse in={filterOpened}>
@@ -132,6 +205,93 @@ const EventListFilter: React.FC<{
                   sx={{ minWidth: "200px" }}
                 />
               </FormControl>
+            </Grid>
+          </Grid>
+          <Grid
+            item
+            container
+            xs={12}
+            alignItems="center"
+            justifyContent="space-between"
+            spacing={1}
+          >
+            <Grid item xs={12} md={1}>
+              <TypographyCaption>{t("common:type")}</TypographyCaption>
+            </Grid>
+            <Grid item xs={12} md={11}>
+              <Grid container spacing={1}>
+                {eventTypes.map((type, index) => (
+                  <Grid key={"event-type-filter-" + index} item>
+                    <Chip
+                      clickable
+                      color={
+                        eventType.includes(type.value as EventType)
+                          ? "primary"
+                          : "default"
+                      }
+                      label={
+                        <Grid container>
+                          <Grid item>{t(type.label)}</Grid>
+                        </Grid>
+                      }
+                      onClick={() =>
+                        handleEventTypeClick(type.value as EventType)
+                      }
+                    />
+                  </Grid>
+                ))}
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid
+            item
+            container
+            xs={12}
+            alignItems="center"
+            justifyContent="space-between"
+            spacing={1}
+          >
+            <Grid item xs={12} md={1}>
+              <TypographyCaption>{t("common:startAt")}</TypographyCaption>
+            </Grid>
+            <Grid item xs={12} md={11}>
+              <Grid container spacing={1}>
+                <Grid item xs={6} md={3}>
+                  <FormControl fullWidth>
+                    <Select
+                      value={startAtType}
+                      onChange={(e) => {
+                        setStartAtType(
+                          e.target.value as EventFilterStartAtType
+                        );
+                        if (!e.target.value) {
+                          setStartAt(undefined);
+                        }
+                      }}
+                    >
+                      <MenuItem value={undefined}>
+                        {t("filter:time.irrelevant")}
+                      </MenuItem>
+                      <MenuItem value="before">
+                        {t("filter:time.before")}
+                      </MenuItem>
+                      <MenuItem value="after">
+                        {t("filter:time.after")}
+                      </MenuItem>
+                    </Select>
+                  </FormControl>
+                </Grid>
+                <Grid item xs={6} md={3}>
+                  <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DatePicker
+                      defaultValue={startAt ? dayjs(startAt) : undefined}
+                      onChange={(newVal) =>
+                        setStartAt(newVal?.valueOf() ?? undefined)
+                      }
+                    />
+                  </LocalizationProvider>
+                </Grid>
+              </Grid>
             </Grid>
           </Grid>
           <Grid
