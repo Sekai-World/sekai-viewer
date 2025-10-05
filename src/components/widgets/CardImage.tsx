e531d394ea22c58a42c5911c4d038d810a76b43e
dnaroma <me@dna.moe>
2025-10-05 14:42:49 +0200

    feat(card): implement card image component with hover effects and lazy loading

--- src/components/widgets/CardImage.tsx
+++ src/components/widgets/CardImage.tsx
@@ -1,21 +1,13 @@
-import React, {
-  Fragment,
-  useCallback,
-  useEffect,
-  useLayoutEffect,
-  useMemo,
-  useRef,
-  useState,
-} from "react";
+import React, { Fragment, useEffect, useMemo, useState } from "react";
 import { ICardInfo } from "../../types.d";
 import {
   cardRarityTypeToRarity,
   getRemoteAssetURL,
   useCachedData,
   useCardType,
-  useRefState,
   useIntersectionObserver,
 } from "../../utils";
+import styles from "./CardImage.module.css";
 
 import rarityNormal from "../../assets/rarity_star_normal.png";
 import rarityAfterTraining from "../../assets/rarity_star_afterTraining.png";
@@ -122,18 +114,8 @@ export const CardImage: React.FC<{ id: number; trained?: boolean }> = ({
 
 export const CardSmallImage: React.FC<{ card: ICardInfo }> = React.memo(
   function CardSmallImage({ card }: { card: ICardInfo }) {
-    const [hoveredArea, setHoveredArea] = useState<number>(0);
-    const [imgLeftX, refImgLeftX, setImgLeftX] = useRefState<number>(-256);
-    const [imgRightX, refImgRightX, setImgRightX] = useRefState<number>(512);
-    // const [requestId, refRequestId, setRequestId] = useRefState<number>(0);
-    const [imgLeftWidth, refImgLeftWidth, setImgLeftWidth] =
-      useRefState<number>(768);
-    const [imgRightWidth, refImgRightWidth, setImgRightWidth] =
-      useRefState<number>(768);
     const { isBirthdayCard, isTrainableCard, isTrainedOnlyCard } =
       useCardType(card);
-
-    const svgElement = useRef<SVGSVGElement>(null);
 
     // Lazy loading setup
     const [containerRef, isVisible] = useIntersectionObserver({
@@ -150,11 +132,6 @@ export const CardSmallImage: React.FC<{ card: ICardInfo }> = React.memo(
             : rarityNormal,
       [isBirthdayCard, isTrainableCard]
     );
-    const animationDuration = 500;
-
-    // useEffect(() => {
-    //   if (cards.length) setCard(cards.find((elem) => elem.id === id));
-    // }, [cards, id]);
 
     const [normalImg, setNormalImg] = useState<string>("");
     const [trainedImg, setTrainedImg] = useState<string>("");
@@ -177,213 +154,101 @@ export const CardSmallImage: React.FC<{ card: ICardInfo }> = React.memo(
       );
     }, [card, isTrainedOnlyCard, isVisible]);
 
-    useLayoutEffect(() => {
-      if (hoveredArea === 2) {
-        const start = performance.now();
-
-        const currLeftX = refImgLeftX.current;
-        const leftXDistance = 0 - refImgLeftX.current;
-        const currLeftWidth = refImgLeftWidth.current;
-        const leftWidthDistance = 940 - currLeftWidth;
-
-        const currRightX = refImgRightX.current;
-        const rightXDistance = 940 - refImgRightX.current;
-        const currRightWidth = refImgRightWidth.current;
-        const rightWidthDistance = 768 - currRightWidth;
-        let requestId = requestAnimationFrame(function animation(time: number) {
-          let timeFraction = (time - start) / animationDuration;
-          if (timeFraction > 1) timeFraction = 1;
-
-          setImgLeftX(Math.floor(currLeftX + leftXDistance * timeFraction));
-          setImgRightX(Math.floor(currRightX + rightXDistance * timeFraction));
-          setImgLeftWidth(
-            Math.floor(currLeftWidth + leftWidthDistance * timeFraction)
-          );
-          setImgRightWidth(
-            Math.floor(currRightWidth + rightWidthDistance * timeFraction)
-          );
-
-          if (timeFraction < 1) {
-            requestId = requestAnimationFrame(animation);
-          } else {
-            cancelAnimationFrame(requestId);
-          }
-        });
-        return () => cancelAnimationFrame(requestId);
-      } else if (hoveredArea === 1) {
-        const start = performance.now();
-
-        const currLeftX = refImgLeftX.current;
-        const leftXDistance = -684 - refImgLeftX.current;
-        const currLeftWidth = refImgLeftWidth.current;
-        const leftWidthDistance = 768 - currLeftWidth;
-
-        const currRightX = refImgRightX.current;
-        const rightXDistance = 84 - refImgRightX.current;
-        const currRightWidth = refImgRightWidth.current;
-        const rightWidthDistance = 940 - currRightWidth;
-        let requestId = requestAnimationFrame(function animation(time: number) {
-          let timeFraction = (time - start) / animationDuration;
-          if (timeFraction > 1) timeFraction = 1;
-
-          setImgLeftX(Math.floor(currLeftX + leftXDistance * timeFraction));
-          setImgRightX(Math.floor(currRightX + rightXDistance * timeFraction));
-          setImgLeftWidth(
-            Math.floor(currLeftWidth + leftWidthDistance * timeFraction)
-          );
-          setImgRightWidth(
-            Math.floor(currRightWidth + rightWidthDistance * timeFraction)
-          );
-
-          if (timeFraction < 1) {
-            requestId = requestAnimationFrame(animation);
-          } else {
-            cancelAnimationFrame(requestId);
-          }
-        });
-        return () => cancelAnimationFrame(requestId);
-      } else if (hoveredArea === 0) {
-        const start = performance.now();
-
-        const currLeftX = refImgLeftX.current;
-        const leftXDistance = -256 - refImgLeftX.current;
-        const currLeftWidth = refImgLeftWidth.current;
-        const leftWidthDistance = 768 - currLeftWidth;
-
-        const currRightX = refImgRightX.current;
-        const rightXDistance = 512 - refImgRightX.current;
-        const currRightWidth = refImgRightWidth.current;
-        const rightWidthDistance = 768 - currRightWidth;
-        let requestId = requestAnimationFrame(function animation(time: number) {
-          let timeFraction = (time - start) / animationDuration;
-          if (timeFraction > 1) timeFraction = 1;
-
-          setImgLeftX(Math.floor(currLeftX + leftXDistance * timeFraction));
-          setImgRightX(Math.floor(currRightX + rightXDistance * timeFraction));
-          setImgLeftWidth(
-            Math.floor(currLeftWidth + leftWidthDistance * timeFraction)
-          );
-          setImgRightWidth(
-            Math.floor(currRightWidth + rightWidthDistance * timeFraction)
-          );
-
-          if (timeFraction < 1) {
-            requestId = requestAnimationFrame(animation);
-          } else {
-            cancelAnimationFrame(requestId);
-          }
-        });
-        return () => cancelAnimationFrame(requestId);
-      }
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [hoveredArea]);
-
-    const handleMoseOver = useCallback(
-      (e: React.MouseEvent<SVGSVGElement, MouseEvent>) => {
-        if (!svgElement.current || isBirthdayCard || isTrainedOnlyCard) {
-          return;
-        }
-        const rect = svgElement.current.getBoundingClientRect();
-        if (e.clientX - rect.left < rect.width / 2) {
-          setHoveredArea(2);
-        } else if (e.clientX - rect.left > rect.width / 2) {
-          setHoveredArea(1);
-        }
-      },
-      [isBirthdayCard, isTrainedOnlyCard]
-    );
-
     return card ? (
       <div ref={containerRef as React.RefObject<HTMLDivElement>}>
         {!isVisible ? (
           // 显示骨架屏直到图片需要加载
           <SvgSkeleton variant="rectangular" />
         ) : (
-          <Svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 1024 576"
-            ref={svgElement}
-            onMouseMove={handleMoseOver}
-            onMouseLeave={() => setHoveredArea(0)}
-          >
-            {isTrainedOnlyCard ? (
-              <image
-                href={trainedImg}
-                x="0"
-                y="0"
-                width="1024"
-                height="576"
-                preserveAspectRatio="xMidYMid slice"
-              ></image>
-            ) : isTrainableCard && !isBirthdayCard ? (
+          <div className={styles.cardContainer}>
+            {/* 悬浮区域 - 必须在最前面 */}
+            {isTrainableCard && !isBirthdayCard && !isTrainedOnlyCard && (
               <Fragment>
-                <svg
-                  x={imgLeftX}
-                  y="0"
-                  width={imgLeftWidth}
-                  height="576"
-                  preserveAspectRatio="xMinYMid slice"
-                  viewBox="0 0 1024 576"
-                  // style={{ pointerEvents: "all" }}
-                >
-                  <image width="1024" height="576" href={normalImg}></image>
-                </svg>
-                <svg
-                  x={imgRightX}
-                  y="0"
-                  width={imgRightWidth}
-                  height="576"
-                  preserveAspectRatio="xMaxYMid slice"
-                  viewBox="0 0 1024 576"
-                  // style={{ pointerEvents: "all" }}
-                >
-                  <image width="1024" height="576" href={trainedImg}></image>
-                </svg>
+                <div
+                  className={`${styles.hoverArea} ${styles.hoverAreaLeft}`}
+                />
+                <div
+                  className={`${styles.hoverArea} ${styles.hoverAreaRight}`}
+                />
               </Fragment>
-            ) : (
-              <image
-                href={normalImg}
-                x="0"
-                y="0"
-                width="1024"
-                height="576"
-                preserveAspectRatio="xMidYMid slice"
-              ></image>
             )}
-            {/* frame */}
-            <image
-              href={
-                cardImageFrameMap[cardRarityTypeToRarity[card.cardRarityType!]]
-              }
-              x="0"
-              y="0"
-              width="1024"
-              height="576"
-            ></image>
-            {/* attr */}
-            <image
-              href={attrIconMap[card.attr]}
-              x="924"
-              y="12"
-              width="88"
-              height="88"
-            />
-            {/* rarity */}
-            {Array.from({
-              length: isBirthdayCard
-                ? 1
-                : cardRarityTypeToRarity[card.cardRarityType!],
-            }).map((_, i) => (
-              <image
-                key={`card-rarity-${i}`}
-                href={rarityIcon}
-                x="16"
-                y={490 - i * 62}
-                width="72"
-                height="70"
-              />
-            ))}
-          </Svg>
+
+            {/* 图片容器 */}
+            <div className={styles.cardContent}>
+              <div className={styles.imageContainer}>
+                {isTrainedOnlyCard ? (
+                  // 只有训练后图片
+                  <img
+                    src={trainedImg}
+                    alt="Card"
+                    className={styles.cardImage}
+                  />
+                ) : isTrainableCard && !isBirthdayCard ? (
+                  // 双图片模式
+                  <Fragment>
+                    <div className={styles.leftImageWrapper}>
+                      <img
+                        src={normalImg}
+                        alt="Card Normal"
+                        className={`${styles.cardImage} ${styles.leftImage}`}
+                      />
+                    </div>
+                    <div className={styles.rightImageWrapper}>
+                      <img
+                        src={trainedImg}
+                        alt="Card Trained"
+                        className={`${styles.cardImage} ${styles.rightImage}`}
+                      />
+                    </div>
+                  </Fragment>
+                ) : (
+                  // 只有普通图片
+                  <img
+                    src={normalImg}
+                    alt="Card"
+                    className={styles.cardImage}
+                  />
+                )}
+              </div>
+
+              {/* 覆盖层元素 */}
+              <div className={styles.overlayContainer}>
+                {/* 卡片边框 */}
+                <img
+                  src={
+                    cardImageFrameMap[
+                      cardRarityTypeToRarity[card.cardRarityType!]
+                    ]
+                  }
+                  alt="Frame"
+                  className={styles.frameOverlay}
+                />
+
+                {/* 属性图标 */}
+                <img
+                  src={attrIconMap[card.attr]}
+                  alt="Attribute"
+                  className={styles.attrIcon}
+                />
+
+                {/* 稀有度星星 */}
+                <div className={styles.rarityContainer}>
+                  {Array.from({
+                    length: isBirthdayCard
+                      ? 1
+                      : cardRarityTypeToRarity[card.cardRarityType!],
+                  }).map((_, i) => (
+                    <img
+                      key={`card-rarity-${i}`}
+                      src={rarityIcon}
+                      alt="Rarity"
+                      className={styles.rarityIcon}
+                    />
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
         )}
       </div>
     ) : (
